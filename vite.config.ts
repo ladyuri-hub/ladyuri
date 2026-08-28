99e38909d73cdef0822ffdd0902a81841cb89170
ladyuri-hub <ladyuree@gmail.com>
2026-08-28 16:20:00 +0900

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,21 @@ import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import {defineConfig} from 'vite';
-import { viteSingleFile } from 'vite-plugin-singlefile';
+
 
 export default defineConfig(() => {
   return {
-    plugins: [react(), tailwindcss(), viteSingleFile()],
+    plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
         '@': path.resolve(__dirname, '.'),
+      },
+    },
+    build: {
+      rollupOptions: {
+        input: {
+          main: path.resolve(__dirname, 'index.html'),
+        },
       },
     },
     server: {
